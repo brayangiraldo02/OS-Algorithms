08433045ef1238ba85e3647497f919d6c17b18ab
Brayan Giraldo <b.catano@utp.edu.co>
2024-03-11 21:10:39 -0500

    Correction burst time Round robin

--- round_robin.py
+++ round_robin.py
@@ -12,6 +12,7 @@ class Round_robin:
     while self.processes:
       current_process = self.processes.pop(0)
 
+      burst_time = current_process.burst_time
       if current_process.arrival_time > next_start_time:
         next_start_time = current_process.arrival_time
         current_process.start_time = next_start_time
@@ -20,7 +21,7 @@ class Round_robin:
       current_process.completion_time = next_start_time
 
       arrival_time = current_process.arrival_time
-      if current_process.burst_time > self.quantum:
+      if current_process.burst_time >= self.quantum:
         current_process.completion_time += self.quantum
         current_process.burst_time -= self.quantum
         next_start_time += self.quantum
@@ -30,22 +31,22 @@ class Round_robin:
         current_process.completion_time += current_process.burst_time
         next_start_time += current_process.burst_time
 
-      burst_time = current_process.burst_time
-      name = current_process.name
-      waiting_time = current_process.start_time - arrival_time
-      system_time = current_process.completion_time - arrival_time
-      priority = 0
+      if burst_time != 0:
+        name = current_process.name
+        waiting_time = current_process.start_time - arrival_time
+        system_time = current_process.completion_time - arrival_time
+        priority = 0
 
-      new_process = Process(name, burst_time, arrival_time, priority)
-      new_process.start_time = current_process.start_time
-      new_process.completion_time = current_process.completion_time
-      new_process.waiting_time = waiting_time
-      new_process.system_time = system_time
+        new_process = Process(name, burst_time, arrival_time, priority)
+        new_process.start_time = current_process.start_time
+        new_process.completion_time = current_process.completion_time
+        new_process.waiting_time = waiting_time
+        new_process.system_time = system_time
 
-      print("--------------------------------------------")
-      print("Process " + new_process.name + " start at: " + str(new_process.start_time))
-      print("Process " + new_process.name + " completed at: " + str(new_process.completion_time))
+        print("--------------------------------------------")
+        print("Process " + new_process.name + " start at: " + str(new_process.start_time))
+        print("Process " + new_process.name + " completed at: " + str(new_process.completion_time))
 
-      self.completed_processes.append(new_process)
+        self.completed_processes.append(new_process)
 
     return self.completed_processes
